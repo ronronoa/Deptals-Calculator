fa8359cc8cd61b83ceebb420a5bd4e5ed3922e1b
ronronoa <116486261+ronronoa@users.noreply.github.com>
2025-05-30 02:20:13 +0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Deptals from './assets/components/Deptals'
+import Navbar from './assets/components/Navbar'
+
+function App() {
+
+  return (
+      <div className="">
+        <Navbar />
+        <Deptals />
+      </div>
+  )
+}
+
+export default App
