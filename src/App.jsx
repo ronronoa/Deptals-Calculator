9700d9b3abd1862e080eb5019981a495c1c16abc
ronronoa <116486261+ronronoa@users.noreply.github.com>
2025-06-12 01:13:56 +0800

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
-
-
+import { useEffect, useState } from 'react'
 import Deptals from './assets/components/Deptals'
 import Footer from './assets/components/Footer'
 import Navbar from './assets/components/Navbar'
+import logo from '/public/images/Psyculator.png'
 
 
 function App() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false)
+    }, 2500)
+
+    return () => clearTimeout(timer)
+  }, [])
 
   return (
       <>
-        <div className="">
-        <Navbar />
-        <Deptals /> 
-        <Footer />
-      </div>
+        {loading ? (
+          <div className="flex items-center justify-center flex-col min-h-screen bg-white">
+            <img src={logo} alt="" width={500} className='animate-pulse'/>
+            <span className="text-lg text-gray-700 text-center">Welcome ateh q!</span>
+            <span className="text-sm text-gray-600 text-center italic animate-bounce">Loading...</span>
+          </div>
+        ) : (
+          <div className="">
+          <Navbar />
+          <Deptals /> 
+          <Footer />
+        </div>
+        )}
       </>
   )
 }
