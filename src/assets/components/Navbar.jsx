89ad1511226f9122e97997570bdffd99d6cc7fae
ronronoa <116486261+ronronoa@users.noreply.github.com>
2025-06-14 23:06:36 +0800

    Add files via upload

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
 import { ChevronDown, ChevronUp } from 'lucide-react';
+import logo from '/public/images/Psyculator.png'
+// eslint-disable-next-line no-unused-vars
+import { motion } from 'motion/react';
 
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false)
@@ -8,10 +11,23 @@ export default function Navbar() {
             setIsOpen(!isOpen)
         }
   return (
-        <nav className='border p-4 shadow-md border-gray-100 md:hidden'>
+        <motion.nav 
+        className='flex justify-between items-center border p-4 shadow-md border-gray-100 md:hidden'
+        initial={{opacity: 0, y: -20}}
+        animate={{opacity: 1, y: 0}}
+        transition={{duration: 1}}
+        >
+            <div className="flex items-center">
+            <img src={logo} alt="" width={30}/>
+            <h1 className='text-xl font-extrabold'>
+                Psyculator
+            </h1>
+            </div>
            <ul className='flex cursor-pointer' onClick={handleClick}>
             <li>About </li>
-            <div>{!isOpen ? <ChevronDown /> : <ChevronUp />}</div>
+            <div className={`transition duration-300 ${isOpen ? '-rotate-180' : ''}`}>
+                <ChevronDown />
+            </div>
            </ul>
 
            <div className={`p-6 border shadow-md rounded ${isOpen ? "absolute top-15 left-0 md:left-15 z-50 bg-gray-100 md:w-md" : "hidden"}`}>
@@ -23,6 +39,6 @@ export default function Navbar() {
             </p>
             <p className='text-sm text-gray-600 mt-2'>The owner and developer of this website <strong>do not collect or have access to any of your information</strong>. Your privacy and data security are completely respected and protected.</p>
            </div>
-        </nav>
+        </motion.nav>
   )
 }
